9cb07effaa053739f00d4309f8988e25b468b84b
AnjanaGunarathna <anjanagunarathna01@gmail.com>
2024-04-22 22:15:19 +0530

    update

--- frontend/src/Components/Slider/Slider.jsx
+++ frontend/src/Components/Slider/Slider.jsx
@@ -13,17 +13,14 @@ function UncontrolledExample() {
       <Carousel.Item>
         <img src={secondimg} alt="Second slide" />
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+       
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img src={thirdimg} alt="Third slide" />
         <Carousel.Caption>
           <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
