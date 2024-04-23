c40b1e642a36b2b83bcc8566a3a44cab3ae20261
AnjanaGunarathna <anjanagunarathna01@gmail.com>
2024-04-23 17:31:10 +0530

    update

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,4 +1,4 @@
 const mongoose = require ("mongoose")
 
 // Database connection with MongoDB
-mongoose.connect("mongodb+srv://rajaratafire:fire200130003314@fireservice.domjhqq.mongodb.net/");
+mongoose.connect("mongodb+srv://rajaratafire:fire200130003314@fireservice.domjhqq.mongodb.net/RajarataFire");
